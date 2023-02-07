3364dfd8d69dcdd4a9f76170327e55fc9f25bbf2
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-02-07 18:48:48 +0800

    Fix user association bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :created_links, class_name: 'Links'
+  has_many :created_links, class_name: "Link"
   has_many :comments
   has_many :commented_links, through: :comments, source: :link
 
