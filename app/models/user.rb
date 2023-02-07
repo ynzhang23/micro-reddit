0f6e73a3d1bc8d55b125b4bfec66418695f26bde
Yining <106020817+ynzhang23@users.noreply.github.com>
2023-02-07 17:06:39 +0800

    Create model and migration for users

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+  has_many :created_links, class_name: 'Links'
+  has_many :comments
+  has_many :commented_links ,through :comments, source :link
+
+  validates :user_name, presence: true, uniqueness: true
+end
